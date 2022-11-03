9a37b2f31e4574b383bc9656dc41561820ae7697
Ziyad Bouazara <ziyad.bouazara.1@ulaval.ca>
2022-11-03 14:53:47 -0700

    Ajout de l'adaptation du sail en fonc de topologie

--- bot.py
+++ bot.py
@@ -20,12 +20,12 @@ class Bot:
         #     return tick.isOver
 
         # Si tout les ports ont ete visite, on retourn au spawn port
-        if (tick.visitedPortIndices.length() == tick.map.ports.length()):
+        if (len(tick.visitedPortIndices) == len(tick.map.ports)):
             closestPort == tick.spawnLocation
 
         # Si on est a un port et qu'il n'a pas encore ete visite OU que on est au port du spawn et que le nombre de ports visite n'est pas nul (i.e. pas au debut de la partie)
         if (tick.currentLocation in tick.map.ports):
-            if (tick.map.ports.index(tick.currentLocation) not in tick.visitedPortIndices) or ((tick.currentLocation == tick.spawnLocation) and (tick.visitedPortIndices is not None)):
+            if (tick.map.ports.index(tick.currentLocation) not in tick.visitedPortIndices) or ((tick.currentLocation == tick.spawnLocation) and (len(tick.visitedPortIndices) > 1)):
                 print(f"Dock at {tick.currentLocation}")
                 return Dock()
 
@@ -46,9 +46,33 @@ class Bot:
 
         if (tick.currentLocation.row < closestPort.row) and (tick.currentLocation.column < closestPort.column):
             # (+1, +1)
-            # Si hauteur_terrain >= eau en SE de currentLoc -> ANCHOR
+            # Si hauteur_terrain >= eau en SE de currentLoc
             if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column + 1]) >= tick.tideSchedule[0]:
-                return Anchor()
+
+                # TEST SI E est clear, si oui, sail E
+                if (tick.map.topology[tick.currentLocation.row][tick.currentLocation.column + 1]) < tick.tideSchedule[0]:
+                    return Sail('E')
+                # TEST SI S est clear, si oui, sail S
+                if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column]) < tick.tideSchedule[0]:
+                    return Sail('S')
+                # TEST SI NE est clear, si oui, sail NE
+                if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column + 1]) < tick.tideSchedule[0]:
+                    return Sail('NE')
+                # TEST SI SW est clear, si oui, sail SW
+                if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column - 1]) < tick.tideSchedule[0]:
+                    return Sail('SW')
+                # TEST SI N est clear, si oui, sail N
+                if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column]) < tick.tideSchedule[0]:
+                    return Sail('N')
+                # TEST SI W est clear, si oui, sail W
+                if (tick.map.topology[tick.currentLocation.row][tick.currentLocation.column - 1]) < tick.tideSchedule[0]:
+                    return Sail('W')
+                # TEST SI NW est clear, si oui, sail NW
+                if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column - 1]) < tick.tideSchedule[0]:
+                    return Sail('NW')
+                else:  # on est donc dans un cul-de-sac, donc anchor
+                    return Anchor()
+
             else:
                 print("Sail SE")
                 return Sail('SE')
@@ -57,7 +81,31 @@ class Bot:
             # (+1, -1)
             # Si hauteur_terrain >= eau en SW de currentLoc -> ANCHOR
             if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column - 1]) >= tick.tideSchedule[0]:
-                return Anchor()
+
+                # TEST SI W est clear, si oui, sail W
+                if (tick.map.topology[tick.currentLocation.row][tick.currentLocation.column - 1]) < tick.tideSchedule[0]:
+                    return Sail('W')
+                # TEST SI S est clear, si oui, sail S
+                if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column]) < tick.tideSchedule[0]:
+                    return Sail('S')
+                # TEST SI NW est clear, si oui, sail NW
+                if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column - 1]) < tick.tideSchedule[0]:
+                    return Sail('NW')
+                # TEST SI SE est clear, si oui, sail SE
+                if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column + 1]) < tick.tideSchedule[0]:
+                    return Sail('SE')
+                # TEST SI N est clear, si oui, sail N
+                if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column]) < tick.tideSchedule[0]:
+                    return Sail('N')
+                # TEST SI E est clear, si oui, sail E
+                if (tick.map.topology[tick.currentLocation.row][tick.currentLocation.column + 1]) < tick.tideSchedule[0]:
+                    return Sail('E')
+                # TEST SI NE est clear, si oui, sail NE
+                if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column + 1]) < tick.tideSchedule[0]:
+                    return Sail('NE')
+                else:
+                    return Anchor()
+
             else:
                 print("Sail SW")
                 return Sail('SW')
@@ -66,7 +114,31 @@ class Bot:
             # (-1, +1)
             # Si hauteur_terrain >= eau en NE de currentLoc -> ANCHOR
             if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column + 1]) >= tick.tideSchedule[0]:
-                return Anchor()
+
+                # TEST SI N est clear, si oui, sail N
+                if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column]) < tick.tideSchedule[0]:
+                    return Sail('N')
+                # TEST SI E est clear, si oui, sail E
+                if (tick.map.topology[tick.currentLocation.row][tick.currentLocation.column + 1]) < tick.tideSchedule[0]:
+                    return Sail('E')
+                # TEST SI SE est clear, si oui, sail SE
+                if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column + 1]) < tick.tideSchedule[0]:
+                    return Sail('SE')
+                # TEST SI NW est clear, si oui, sail NW
+                if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column - 1]) < tick.tideSchedule[0]:
+                    return Sail('NW')
+                # TEST SI W est clear, si oui, sail W
+                if (tick.map.topology[tick.currentLocation.row][tick.currentLocation.column - 1]) < tick.tideSchedule[0]:
+                    return Sail('W')
+                # TEST SI S est clear, si oui, sail S
+                if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column]) < tick.tideSchedule[0]:
+                    return Sail('S')
+                # TEST SI SW est clear, si oui, sail SW
+                if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column - 1]) < tick.tideSchedule[0]:
+                    return Sail('SW')
+                else:
+                    return Anchor()
+
             else:
                 print("Sail NE")
                 return Sail('NE')
@@ -75,7 +147,31 @@ class Bot:
             # (-1, -1)
             # Si hauteur_terrain >= eau en NW de currentLoc -> ANCHOR
             if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column - 1]) >= tick.tideSchedule[0]:
-                return Anchor()
+
+                # TEST SI N est clear, si oui, sail N
+                if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column]) < tick.tideSchedule[0]:
+                    return Sail('N')
+                # TEST SI W est clear, si oui, sail W
+                if (tick.map.topology[tick.currentLocation.row][tick.currentLocation.column - 1]) < tick.tideSchedule[0]:
+                    return Sail('W')
+                # TEST SI SW est clear, si oui, sail SW
+                if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column - 1]) < tick.tideSchedule[0]:
+                    return Sail('SW')
+                # TEST SI NE est clear, si oui, sail NE
+                if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column + 1]) < tick.tideSchedule[0]:
+                    return Sail('NE')
+                # TEST SI E est clear, si oui, sail E
+                if (tick.map.topology[tick.currentLocation.row][tick.currentLocation.column + 1]) < tick.tideSchedule[0]:
+                    return Sail('E')
+                # TEST SI S est clear, si oui, sail S
+                if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column]) < tick.tideSchedule[0]:
+                    return Sail('S')
+                # TEST SI SE est clear, si oui, sail SE
+                if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column + 1]) < tick.tideSchedule[0]:
+                    return Sail('SE')
+                else:
+                    return Anchor()
+
             else:
                 print("Sail NW")
                 return Sail('NW')
@@ -84,7 +180,31 @@ class Bot:
             # (+1, 0)
             # Si hauteur_terrain >= eau en S de currentLoc -> ANCHOR
             if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column]) >= tick.tideSchedule[0]:
-                return Anchor()
+
+                # TEST SI SE est clear, si oui, sail SE
+                if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column + 1]) < tick.tideSchedule[0]:
+                    return Sail('SE')
+                # TEST SI SW est clear, si oui, sail SW
+                if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column - 1]) < tick.tideSchedule[0]:
+                    return Sail('SW')
+                # TEST SI W est clear, si oui, sail W
+                if (tick.map.topology[tick.currentLocation.row][tick.currentLocation.column - 1]) < tick.tideSchedule[0]:
+                    return Sail('W')
+                # TEST SI E est clear, si oui, sail E
+                if (tick.map.topology[tick.currentLocation.row][tick.currentLocation.column + 1]) < tick.tideSchedule[0]:
+                    return Sail('E')
+                # TEST SI NE est clear, si oui, sail NE
+                if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column + 1]) < tick.tideSchedule[0]:
+                    return Sail('NE')
+                # TEST SI NW est clear, si oui, sail NW
+                if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column - 1]) < tick.tideSchedule[0]:
+                    return Sail('NW')
+                # TEST SI N est clear, si oui, sail N
+                if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column]) < tick.tideSchedule[0]:
+                    return Sail('N')
+                else:
+                    return Anchor()
+
             else:
                 print("Sail S")
                 return Sail('S')
@@ -93,7 +213,31 @@ class Bot:
             # (-1, 0)
             # Si hauteur_terrain >= eau en N de currentLoc -> ANCHOR
             if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column]) >= tick.tideSchedule[0]:
-                return Anchor()
+
+                # TEST SI NE est clear, si oui, sail NE
+                if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column + 1]) < tick.tideSchedule[0]:
+                    return Sail('NE')
+                # TEST SI NW est clear, si oui, sail NW
+                if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column - 1]) < tick.tideSchedule[0]:
+                    return Sail('NW')
+                # TEST SI W est clear, si oui, sail W
+                if (tick.map.topology[tick.currentLocation.row][tick.currentLocation.column - 1]) < tick.tideSchedule[0]:
+                    return Sail('W')
+                # TEST SI E est clear, si oui, sail E
+                if (tick.map.topology[tick.currentLocation.row][tick.currentLocation.column + 1]) < tick.tideSchedule[0]:
+                    return Sail('E')
+                # TEST SI SE est clear, si oui, sail SE
+                if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column + 1]) < tick.tideSchedule[0]:
+                    return Sail('SE')
+                # TEST SI SW est clear, si oui, sail SW
+                if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column - 1]) < tick.tideSchedule[0]:
+                    return Sail('SW')
+                # TEST SI S est clear, si oui, sail S
+                if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column]) < tick.tideSchedule[0]:
+                    return Sail('S')
+                else:
+                    return Anchor()
+
             else:
                 print("Sail N")
                 return Sail('N')
@@ -102,7 +246,31 @@ class Bot:
             # (0, +1)
             # Si hauteur_terrain >= eau en E de currentLoc -> ANCHOR
             if (tick.map.topology[tick.currentLocation.row][tick.currentLocation.column + 1]) >= tick.tideSchedule[0]:
-                return Anchor()
+
+                # TEST SI NE est clear, si oui, sail NE
+                if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column + 1]) < tick.tideSchedule[0]:
+                    return Sail('NE')
+                # TEST SI SE est clear, si oui, sail SE
+                if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column + 1]) < tick.tideSchedule[0]:
+                    return Sail('SE')
+                # TEST SI S est clear, si oui, sail S
+                if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column]) < tick.tideSchedule[0]:
+                    return Sail('S')
+                # TEST SI N est clear, si oui, sail N
+                if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column]) < tick.tideSchedule[0]:
+                    return Sail('N')
+                # TEST SI NW est clear, si oui, sail NW
+                if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column - 1]) < tick.tideSchedule[0]:
+                    return Sail('NW')
+                # TEST SI SW est clear, si oui, sail SW
+                if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column - 1]) < tick.tideSchedule[0]:
+                    return Sail('SW')
+                # TEST SI W est clear, si oui, sail W
+                if (tick.map.topology[tick.currentLocation.row][tick.currentLocation.column - 1]) < tick.tideSchedule[0]:
+                    return Sail('W')
+                else:
+                    return Anchor()
+
             else:
                 print("Sail E")
                 return Sail('E')
@@ -111,7 +279,31 @@ class Bot:
             # (0, -1)
             # Si hauteur_terrain >= eau en W de currentLoc -> ANCHOR
             if (tick.map.topology[tick.currentLocation.row][tick.currentLocation.column - 1]) >= tick.tideSchedule[0]:
-                return Anchor()
+
+                # TEST SI NW est clear, si oui, sail NW
+                if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column - 1]) < tick.tideSchedule[0]:
+                    return Sail('NW')
+                # TEST SI SW est clear, si oui, sail SW
+                if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column - 1]) < tick.tideSchedule[0]:
+                    return Sail('SW')
+                # TEST SI S est clear, si oui, sail S
+                if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column]) < tick.tideSchedule[0]:
+                    return Sail('S')
+                # TEST SI N est clear, si oui, sail N
+                if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column]) < tick.tideSchedule[0]:
+                    return Sail('N')
+                # TEST SI NE est clear, si oui, sail NE
+                if (tick.map.topology[tick.currentLocation.row - 1][tick.currentLocation.column + 1]) < tick.tideSchedule[0]:
+                    return Sail('NE')
+                # TEST SI SE est clear, si oui, sail SE
+                if (tick.map.topology[tick.currentLocation.row + 1][tick.currentLocation.column + 1]) < tick.tideSchedule[0]:
+                    return Sail('SE')
+                # TEST SI E est clear, si oui, sail E
+                if (tick.map.topology[tick.currentLocation.row][tick.currentLocation.column + 1]) < tick.tideSchedule[0]:
+                    return Sail('E')
+                else:
+                    return Anchor()
+
             else:
                 print("Sail W")
                 return Sail('W')
